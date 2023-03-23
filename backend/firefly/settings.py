00378f4b7d931c1a0c3bdc394e9f7a169634549e
julianv <julianv302@gmail.com>
2023-03-22 22:31:49 -0500

    Se amplia la expiracion de token JWT

--- backend/firefly/settings.py
+++ backend/firefly/settings.py
@@ -178,7 +178,7 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
     'BLACKLIST_AFTER_ROTATION': False,
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=40),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
 DJOSER = {
