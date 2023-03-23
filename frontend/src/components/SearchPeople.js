a6b0f71a93bd3ea1015f377df35db7993f09b37a
ANGEL DAVID MARTÍNEZ DORIA <angelydiego2323@gmail.com>
2023-03-22 22:32:32 -0500

    Se agrega la personalizacion de la barra de busqueda y reacticons

--- frontend/src/components/SearchPeople.js
+++ frontend/src/components/SearchPeople.js
@@ -1,30 +1,34 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import axios from 'axios'
+import { MdOutlinePersonSearch } from 'react-icons/md';
 
-function SearchPeople(){
+function SearchPeople() {
     const [nombre, setNombre] = useState('');
     const [usuarios, setUsuarios] = useState([]);
     async function handleChange(event) {
         setNombre(event.target.value);
         const res = await axios(`${process.env.REACT_APP_API_URL}/search/?name=${event.target.value}`)
-        .then(response => response.data.users)
-        .then(data => setUsuarios(data));
+            .then(response => response.data.users)
+            .then(data => setUsuarios(data));
     }
     console.log('nombre', nombre)
     console.log('usuarios', usuarios);
     return (
-        <div>
-        <input type="text" value={nombre} onChange={handleChange} />
-        {nombre !== "" &&(
-            <ul>
-                {usuarios.map((usuario) => (
-                    <li key={usuario.id}>
-                        <a href={`/profile/${usuario.id}/`}> {usuario.name} - {usuario.age} </a>
-                    </li>
-                ))}
-            </ul>
-        )}
+        <div className="Barra">
+            <MdOutlinePersonSearch class="Icono" />
+            <input type="text" value={nombre} onChange={handleChange} placeholder="Buscar Usuarios..." />
+            {nombre !== "" && (
+                <div className="Espacio">
+                    <ul>
+                        {usuarios.map((usuario) => (
+                            <li key={usuario.id}>
+                                <a href={`/profile/${usuario.id}/`}> {usuario.name} - {usuario.age} </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
         </div>
     )
 }
-export { SearchPeople } 
+export { SearchPeople }
