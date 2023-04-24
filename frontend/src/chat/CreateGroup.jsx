1a346382d1b991040a95cc5d0ce43149870b9091
Hobarlan <huparelaa@eafit.edu.co>
2023-04-23 21:47:44 -0500

    Ventana emergente para añadir amigos creada

--- frontend/src/chat/CreateGroup.jsx
+++ frontend/src/chat/CreateGroup.jsx
@@ -1,106 +1,133 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-function CreateGroup({ setModalIsOpen,config }) {
-//   const [roomName, setRoomName] = useState("");
+import {Navigate, Link} from 'react-router-dom'
 
-// //   const personas = 
+function CreateGroup({ setModalIsOpen, config }) {
+  const [roomName, setRoomName] = useState("");
+  const [filtro, setFiltro] = useState("");
+  const [amigos, setAmigos] = useState(null);
+  const [personasSeleccionadas, setPersonasSeleccionadas] = useState({});
+  const [redirectToChatRoom, setRedirectToChatRoom] = useState(false);
+  const [idRoom, setIdRoom] = useState("");
+  useEffect(() => {
+    async function getFriends() {
+      const response = await axios.get(
+        `${process.env.REACT_APP_API_URL}/api/get_friends/`,
+        {
+          headers: {
+            Authorization: `JWT ${localStorage.getItem("access")}`,
+          },
+        }
+      );
+      setAmigos(response.data.friends);
+    }
+    getFriends();
+  }, []);
 
-//   const handleAddMember = async (event) => {
-//     event.preventDefault();
-//   };
-//   const addMembers = async (roomName, members) => {
-//     const config = {
-//       headers: {
-//         "Content-type": "application/json",
-//         Authorization: `JWT ${localStorage.getItem("access")}`,
-//         Accept: "application/json",
-//       },
-//     };
-//     const url = `${process.env.REACT_APP_API_URL}/api/room/`;
+  const handleAddMember = async (event) => {
+    event.prevetnDefault();
+    
+  };
 
-//     try {
-//       const response = await axios.post(
-//         url,
-//         {
-//           room_name: roomName,
-//           members: members,
-//         },
-//         config
-//       );
+  async function createRoom (){
+    const url = `${process.env.REACT_APP_API_URL}/api/room/`;
 
-//       return response.data;
-//     } catch (error) {
-//       console.error(error);
-//       return null;
-//     }
-//   };
+    try {
+      const response = await axios.post(
+        url,
+        {
+          room_name: roomName,
+          members: personasSeleccionadas,
+        },
+        config
+      );
+        
+      
+      setIdRoom(response.data.id_room)
+    
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  };
 
-//   const listaFiltrada =
-//     filtro === ""
-//       ? personas
-//       : personas.filter((persona) =>
-//           persona.toLowerCase().includes(filtro.toLowerCase())
-//         );
+  const handleSeleccionarPersona = (nombrePersona) => {
+    setPersonasSeleccionadas({
+      ...personasSeleccionadas,
+      [nombrePersona]: !personasSeleccionadas[nombrePersona],
+    });
+  };
 
-//   const listaSugerencias = listaFiltrada.map((persona) => (
-//     <div
-//       key={persona}
-//       className="flex items-center cursor-pointer"
-//       onClick={() => handleSeleccionarPersona(persona)}
-//     >
-//       <span
-//         className={`w-full p-2 rounded-md ${
-//           personasSeleccionadasLocal[persona] ? "bg-blue-500" : "bg-gray-100"
-//         }`}
-//       >
-//         {persona}
-//       </span>
-//     </div>
-//   ));
+  const listaFiltrada =
+    filtro === ""
+      ? amigos
+      : amigos.filter((persona) =>
+          persona.toLowerCase().includes(filtro.toLowerCase())
+        );
 
-  return (<></>
-    // <div className="bg-dark-purple rounded-lg w-1/2 p-6">
-    //   <button
-    //     className="absolute top-0 right-0 m-4 text-gray-700 hover:text-gray-900"
-    //     onClick={() => setModalIsOpen(false)}
-    //   ></button>
-    //   <h2 className="text-lg font-medium mb-4 text-white">Crear grupo</h2>
-    //   <form onSubmit={handleAddMember}>
-    //     <ul>
-    //       <li>
-    //         <label htmlFor="room_name" className="text-white pr-3">
-    //           Nombre del grupo:
-    //         </label>
-    //         <input
-    //           type="text"
-    //           id="room_name"
-    //           onChange={(event) => setRoomName(event.target.value)}
-    //           className="pl-1 border border-black"
-    //         />
-    //         <div></div>
-    //         <label htmlFor="members" className="text-white pr-3">
-    //           Miembros
-    //         </label>
-    //         <input type="" />
-    //       </li>
-    //     </ul>
+  const listaSugerencias = amigos
+    ? listaFiltrada.map((persona) => (
+        <div
+          key={persona}
+          className="flex items-center cursor-pointer"
+          onClick={() => handleSeleccionarPersona(persona)}
+        >
+          <input
+            type="checkbox"
+            checked={personasSeleccionadas[persona]}
+            onChange={() => handleSeleccionarPersona(persona)}
+          />
+          <span className="text-white pl-6">{persona[0]}</span>
+        </div>
+      ))
+    : null;
 
-    //     <button
-    //       type="submit"
-    //       className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-4"
-    //     >
-    //       Crear grupo
-    //     </button>
+  return (
+    <div className="bg-dark-purple rounded-lg w-1/2 p-6">
+      <button
+        className="absolute top-0 right-0 m-4 text-gray-700 hover:text-gray-900"
+        onClick={() => setModalIsOpen(false)}
+      ></button>
+      <h2 className="text-lg font-medium mb-4 text-white">Crear grupo</h2>
 
-    //     <button
-    //       type="button"
-    //       className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-4 ml-4"
-    //       onClick={() => setModalIsOpen(false)}
-    //     >
-    //       Cancelar
-    //     </button>
-    //   </form>
-    // </div>
+      <form onSubmit={handleAddMember}>
+        <ul>
+          <li>
+            <label htmlFor="room_name" className="text-white pr-3">
+              Nombre del grupo:
+            </label>
+            <input
+              type="text"
+              id="room_name"
+              onChange={(event) => setRoomName(event.target.value)}
+              className="pl-1 border border-black"
+            />
+            <div></div>
+            <label htmlFor="members" className="text-white pr-3">
+              Selecciona a tus amigos:
+            </label>
+            <div className="flex items-center">
+              
+            </div>
+            <div className="mt-2">{listaSugerencias}</div>
+          </li>
+        </ul>
+          <button
+            type="submit"
+            className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-4"
+            onClick={createRoom}
+          >
+            Crear grupo
+          </button>
+        <button
+          type="button"
+          className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-4 ml-4"
+          onClick={() => setModalIsOpen(false)}
+        >
+          Cancelar
+        </button>
+      </form>
+    </div>
   );
 }
 
