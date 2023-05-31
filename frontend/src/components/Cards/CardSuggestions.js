952371abbb062d73a4561e539d3471142421610e
dgonzalezt <dgonzalez2@eafit.edu.co>
2023-05-30 19:43:21 -0500

    Se modifica el diseño en el dashboard
    Se modifica el diseño en el dashboard

--- frontend/src/components/Cards/CardSuggestions.js
+++ frontend/src/components/Cards/CardSuggestions.js
@@ -40,10 +40,10 @@ const SuggestedGames = () => {
     }
     return (
 <div className="text-center">
-  <h3 className="text-white mb-4 font-bold">Juegos sugeridos:</h3>
-  <div className="grid gap-4 lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1">
+  <h3 className="text-white mb-4 font-bold mt-5">Juegos sugeridos:</h3>
+  <div className="grid gap-3 lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 ml-10 mr-10">
     {suggestedGames.map((game) => (
-      <div key={game.id_game} className="text-white bg-friend-list p-3 box-border shadow-md">
+      <div key={game.id_game} className="text-white bg-friend-list w-76 p-3 box-border shadow-md">
         <img src={game.img} className="w-40 h-32 object-cover mx-auto mb-4" alt={game.title} />
         <div className="card-body">
           <h5 className="my-2 truncate">{game.title}</h5>
